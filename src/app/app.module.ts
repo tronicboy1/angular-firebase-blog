457e8d7ab63e2c1dd58547df651d114f56325b15
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-13 15:58:04 +0900

    add firebase and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
