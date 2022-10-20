bb988daaa676e39f88bda8c0adbe078269bff516
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-20 17:26:27 +0900

    add dev firebase app check settings

--- src/firebase/app-check-module.ts
+++ src/firebase/app-check-module.ts
@@ -1,9 +1,19 @@
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 import { firebaseApp } from "./index";
+import { environment } from "../environments/environment";
 
 const reCAPTCHA_PUBLIC_KEY = "6LfZi5UiAAAAAA9IqaPiw_C-wn17mziJTJWduHZX";
 
-export const startAppCheck = () => initializeAppCheck(firebaseApp, {
-  provider: new ReCaptchaV3Provider(reCAPTCHA_PUBLIC_KEY),
-  isTokenAutoRefreshEnabled: true,
-});
+if (!environment.production) {
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
+
+export const startAppCheck = () =>
+  initializeAppCheck(firebaseApp, {
+    provider: new ReCaptchaV3Provider(reCAPTCHA_PUBLIC_KEY),
+    isTokenAutoRefreshEnabled: true,
+  });
+
+declare var self: {
+  FIREBASE_APPCHECK_DEBUG_TOKEN: boolean | undefined;
+};
