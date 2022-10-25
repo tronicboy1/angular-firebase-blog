8c5bebfbcec443f4f478800284e80ce66f9d7658
tronicboy1 <mayeraus@icloud.com>
2022-10-25 17:47:20 +0900

    setup new firebase account

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,13 +1,19 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const config = {
-  projectId: "austin-blog-965da",
-  appId: "1:262418756090:web:ff1629670efe372c017443",
-  storageBucket: "austin-blog-965da.appspot.com",
-  locationId: "asia-east2",
-  apiKey: "AIzaSyBEMXKxQ-R7FnO3KVbnMZpSdluPFcp-lyY",
-  authDomain: "austin-blog-965da.firebaseapp.com",
-  messagingSenderId: "262418756090",
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfHOnbpqeOBdL3ZY0CohUj2qCbnf2IEQc",
+  authDomain: "austin-mayer-blog.firebaseapp.com",
+  projectId: "austin-mayer-blog",
+  storageBucket: "austin-mayer-blog.appspot.com",
+  messagingSenderId: "353824563902",
+  appId: "1:353824563902:web:969ffaf048dd3a15b1b4ef",
+  measurementId: "G-3PZH7TSRYN",
 };
 
-export const firebaseApp = initializeApp(config);
+export const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
